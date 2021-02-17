1fa7b2bad6d32e2f1da1af3daeee94dd30908ad2
samuel <ssacrist@c1r11s3.42madrid.com>
2021-02-17 20:26:30 +0100

    Improve ft_strcpy

--- ft_strcpy.s
+++ ft_strcpy.s
@@ -6,7 +6,12 @@
 				global		_ft_strcpy
 
 _ft_strcpy:
-				xor		rax, rax
-				mov		rdi, rsi
-				mov		rax, rdi
+				cmp		byte[rsi], 0x0
+				jz		exit
+				movsd					; copy from DS:(E)SI to  ES:(E)DI (de rsi a rdi) movsb: byte a byte - movsw: word a word - movsd: doubleword a doubleword 
+;				mov		rbx, [rsi]
+;				xchg	rax, rbx
+				jmp		_ft_strcpy
+exit:
+				mov		byte[rdi], 0x0
 				ret
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ssacrist <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/26 16:21:33 by ssacrist          #+#    #+#             */
-/*   Updated: 2021/02/17 18:23:47 by ssacrist         ###   ########.fr       */
+/*   Updated: 2021/02/17 19:57:02 by ssacrist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,22 +58,34 @@ void	test_ft_strcpy()
 {
 	char	*src;
 	char	dst[64];
+	char	dst1[64];
+	char	dst2[64];
+//	char	dst3[64];
+	char	*temp;
 
 	print_title("strcpy vs. ft_strcpy");
 	printf("%s", _YELLOW);
 	
-	src = "hola";
-	print_equal_string(strcpy(dst, src), ft_strcpy(dst, src));
+	src = "hola k";
+	temp = src;
+	ft_strcpy(dst, src);
+	print_equal_string(temp, dst);
 
 	src = "";
-	print_equal_string(strcpy(dst, src), ft_strcpy(dst, src));
-
-	src = "0123456789abcedef";
-	print_equal_string(strcpy(dst, src), ft_strcpy(dst, src));
-
-//	src = NULL;
-//	printf("src: '%s'; dest: '%s' ('%s')\n", src, strcpy(dst, src), ft_strcpy(dst, src));
-
+	temp = src;
+	ft_strcpy(dst1, src);
+	print_equal_string(temp, dst1);
+	
+	src = "0123456789abcedef kjjhljkhjkl hlkjhljkhljk hlkjhljkhljk ljkhljkhlkj kjlhlkjhljk hljkhlkjhkljh ljkh ljkhlkjh lkjhlkjhlkj lkjh lkjhlkjh ljkh lkjh ";
+	temp = src;
+	ft_strcpy(dst2, src);
+	print_equal_string(temp, dst2);
+	
+/*	src = NULL;
+	temp = src;
+	ft_strcpy(dst3, src);
+	print_equal_string(temp, dst3);
+*/
 }
 
 int		main()
